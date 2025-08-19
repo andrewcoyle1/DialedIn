858eb98344bd0b0fb4a9e448cd82565ca150542e
Andrew Coyle <andrewcoyle.1@outlook.com>
2025-08-19 14:07:53 +0100

    Initial Commit

---
+++ DialedIn/DialedInApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialedInApp.swift
+//  DialedIn
+//
+//  Created by Andrew Coyle on 19/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct DialedInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
